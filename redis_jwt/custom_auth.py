eaf0d6d0831ae1989a2e1e410bcc19576e64c82a
Verchenko <via_atm@mail.ru>
2025-03-27 22:25:16 +0300

    add a README file and fix access without token in whitelist

--- redis_jwt/custom_auth.py
+++ redis_jwt/custom_auth.py
@@ -11,7 +11,10 @@ class CustomJWTAuth(JWTAuthentication):
         if authenticate_info is None:
             return None
         user, token = authenticate_info
-        check_in_blacklist = caches['blacklist'].get(str(token), None)
+        check_in_blacklist = caches['blacklist'].get(str(token), False)
         if check_in_blacklist:
+            return None
+        check_in_whitelist = caches['whitelist'].get(str(token), False)
+        if not check_in_whitelist:
             return None
         return user, token
